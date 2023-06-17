b87e715977e9fffb0cd3f21a9fa1335bf0abda2b
Alex Kumancev <kumancev1@gmail.com>
2023-06-18 00:11:57 +0300

    feat: add routes

--- src/components/StartForm/StartForm.tsx
+++ src/components/StartForm/StartForm.tsx
@@ -1,11 +1,9 @@
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import './index.module.scss'
 
 const MyForm = () => {
-  const {
-    register,
-    handleSubmit,
-  } = useForm()
+  const { register, handleSubmit } = useForm()
 
   const onSubmit = (data: any) => {
     console.log(data)
@@ -18,7 +16,9 @@ const MyForm = () => {
         <input {...register('phone')} placeholder="+7 900 201-23-33" />
         <label htmlFor="email">Email</label>
         <input {...register('email')} placeholder="kumancev1@gmail.com" />
-        <button type="submit">Submit</button>
+        <button>
+          <Link to={`create`}>Submit</Link>
+        </button>
       </form>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Form from './sections/Form/Form.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles/index.scss'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/create',
+    element: <Form />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
+  <RouterProvider router={router} />
 )
