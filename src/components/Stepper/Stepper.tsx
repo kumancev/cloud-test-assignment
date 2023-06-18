f014283f42d2b1281db68220bb3a09cc7514ef88
Alex Kumancev <kumancev1@gmail.com>
2023-06-19 01:14:16 +0300

    fix: remove ability to change status when click stepper

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,25 +1,15 @@
 import checkIcon from '@assets/check-icon.svg'
 import dotIcon from '@assets/dot-icon.svg'
-import { useAppDispatch, useAppSelector } from '@app/hooks'
-import { updateStepState } from '@features/stepper/stepperSlice'
+import { useAppSelector } from '@app/hooks'
 import './index.css'
 
 const Stepper = () => {
   const steps = useAppSelector((state) => state.stepper.steps)
-  const dispatch = useAppDispatch()
-
-  const handleStepClick = (stepId: number) => {
-    dispatch(updateStepState({ stepId, newState: 'completed' }))
-  }
 
   return (
     <div className="stepper-wrapper">
       {steps.map((step) => (
-        <div
-          key={step.id}
-          className={`stepper-item ${step.state}`}
-          onClick={() => handleStepClick(step.id)}
-        >
+        <div key={step.id} className={`stepper-item ${step.state}`}>
           <div className="step-counter">
             {step.state === 'completed' && <img src={checkIcon} alt="check" />}
             {step.state === 'active' && <img src={dotIcon} alt="dot" />}
